aa2e422603488761f64fd0c27001c172cc4573fd
Sunderland93 <samoilov.lex@gmail.com>
2022-05-27 00:05:31 +0400

    Fix invisible cursor in VirtualBox and QEMU/KVM

--- etc/profile.d/sway.sh
+++ etc/profile.d/sway.sh
@@ -12,3 +12,19 @@ export QT_QPA_PLATFORMTHEME=qt5ct
 
 #Java XWayland blank screens fix
 export _JAVA_AWT_WM_NONREPARENTING=1
+
+#Check if system is running on VM
+
+if [ $(systemd-detect-virt) = none ]; then
+	 echo "No VM is detected"
+elif
+   [ $(systemd-detect-virt) = oracle ]; then
+     echo "Running in VirtualBox. Disabling hardware cursors"
+     export WLR_NO_HARDWARE_CURSORS=1
+elif
+   [ $(systemd-detect-virt) = KVM ]; then
+     echo "Running in QEMU/KVM. Disabling hardware cursors"
+     export WLR_NO_HARDWARE_CURSORS=1
+else
+     echo "Unknown VM. If your cursor is invisible, set WLR_NO_HARDWARE_CURSORS to 1"
+fi
