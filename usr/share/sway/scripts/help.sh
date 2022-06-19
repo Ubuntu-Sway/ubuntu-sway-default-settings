119ebdc82e2694b4ea7f37eac74302f3e880b56a
Sunderland93 <samoilov.lex@gmail.com>
2022-06-19 14:25:32 +0400

    use posix compliant syntax in help.sh

--- usr/share/sway/scripts/help.sh
+++ usr/share/sway/scripts/help.sh
@@ -1,8 +1,7 @@
-#!/bin/bash
+#!/bin/sh
 set -x
 # toggles the help wrapper state
 
-OUTPUTS=$(swaymsg -t get_outputs --raw | jq -r '.[].name')
 VISIBILITY_SIGNAL=30
 QUIT_SIGNAL=31
 
@@ -10,7 +9,7 @@ if [[ "$1" == "--toggle" ]]; then
     pkill -f -${VISIBILITY_SIGNAL} nwg-wrapper
 else
     pkill -f -${QUIT_SIGNAL} nwg-wrapper
-    for o in "$OUTPUTS"; do
-        nwg-wrapper -o $o -sv ${VISIBILITY_SIGNAL} -sq ${QUIT_SIGNAL} -s help.sh -p left -a end &
+    for output in $(swaymsg -t get_outputs --raw | jq -r '.[].name'); do
+       nwg-wrapper -o $output -sv ${VISIBILITY_SIGNAL} -sq ${QUIT_SIGNAL} -s help.sh -p left -a end &
     done
 fi
