37a8f4d8eaa971abe089bf96f262fe7e89c06d9c
Sunderland93 <samoilov.lex@gmail.com>
2022-06-25 15:55:58 +0400

    Set Ubuntu Sway specific config dir

--- etc/profile.d/sway.sh
+++ etc/profile.d/sway.sh
@@ -1,6 +1,9 @@
 # Set XDG_CURRENT_DESKTOP to Sway (for screencasting and screensharing capabilities)
 export XDG_CURRENT_DESKTOP=sway
 
+# Ubuntu Sway specific config dir
+export XDG_CONFIG_DIRS=/etc/xdg/xdg-ubuntusway:/etc/xdg
+
 # Force Wayland for Mozilla Firefox
 export MOZ_ENABLE_WAYLAND=1
 export MOZ_DBUS_REMOTE=1
