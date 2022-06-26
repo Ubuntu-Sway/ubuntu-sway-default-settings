448aefd59a37611da18e7ce3338eadbb8377632c
Sunderland93 <samoilov.lex@gmail.com>
2022-06-26 22:28:44 +0400

    Fix nwg-wrapper helper script

--- usr/share/sway/scripts/help.sh
+++ usr/share/sway/scripts/help.sh
@@ -5,11 +5,11 @@ set -x
 VISIBILITY_SIGNAL=30
 QUIT_SIGNAL=31
 
-if [[ "$1" == "--toggle" ]]; then
+if [ "$1" = "--toggle" ]; then
     pkill -f -${VISIBILITY_SIGNAL} nwg-wrapper
 else
     pkill -f -${QUIT_SIGNAL} nwg-wrapper
     for output in $(swaymsg -t get_outputs --raw | jq -r '.[].name'); do
-       nwg-wrapper -o $output -sv ${VISIBILITY_SIGNAL} -sq ${QUIT_SIGNAL} -s help.sh -p left -a end &
+        nwg-wrapper -o "$output" -sv ${VISIBILITY_SIGNAL} -sq ${QUIT_SIGNAL} -s help.sh -p left -a end &
     done
 fi
