e1ac540dbe991c54374baf2791b195c460e0f395
Sunderland93 <samoilov.lex@gmail.com>
2022-12-24 16:17:08 +0400

    Fix typo in waybar script

--- usr/share/sway/scripts/waybar.sh
+++ usr/share/sway/scripts/waybar.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # wrapper script for waybar with args, see https://github.com/swaywm/sway/issues/5724
 
-USER_CONFIG_PATH=$HOME/.config/waybar/config.jsonc
+USER_CONFIG_PATH=$HOME/.config/waybar/config.jsonrc
 USER_STYLE_PATH=$HOME/.config/waybar/style.css
 
 if [ -f $USER_CONFIG_PATH ]; then
