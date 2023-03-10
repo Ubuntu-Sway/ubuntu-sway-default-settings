f734d74c9af86d367151b1192af706728751fa49
Sunderland93 <samoilov.lex@gmail.com>
2023-03-10 18:35:37 +0400

    Fallback to XCB QPA if Wayland is not available (fixes Appimage)

--- etc/profile.d/sway.sh
+++ etc/profile.d/sway.sh
@@ -9,7 +9,7 @@ export MOZ_ENABLE_WAYLAND=1
 export MOZ_DBUS_REMOTE=1
 
 # Force Wayland for Qt apps
-export QT_QPA_PLATFORM="wayland"
+export QT_QPA_PLATFORM="wayland;xcb"
 export QT_QPA_PLATFORMTHEME=qt5ct
 
 # Force Wayland for EFL (Enlightenment) apps
