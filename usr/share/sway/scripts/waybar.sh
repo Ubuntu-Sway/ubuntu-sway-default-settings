291ce0fa7d6323e1d9bee0dd0be57506f285e032
Sunderland93 <samoilov.lex@gmail.com>
2023-03-12 21:31:04 +0400

    Typo

--- usr/share/sway/scripts/waybar.sh
+++ usr/share/sway/scripts/waybar.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # wrapper script for waybar with args, see https://github.com/swaywm/sway/issues/5724
 
-USER_CONFIG_PATH=$HOME/.config/waybar/config.jsonrc
+USER_CONFIG_PATH=$HOME/.config/waybar/config.jsonc
 USER_STYLE_PATH=$HOME/.config/waybar/style.css
 
 if [ -f "$USER_CONFIG_PATH" ]; then
@@ -12,4 +12,4 @@ if [ -f "$USER_STYLE_PATH" ]; then
   USER_STYLE=$USER_STYLE_PATH
 fi
 
-waybar -c "${USER_CONFIG:-"/etc/xdg/xdg-ubuntusway/waybar/config.jsonrc"}" -s "${USER_STYLE:-"/etc/xdg/xdg-ubuntusway/waybar/style.css"}" &
+waybar -c "${USER_CONFIG:-"/etc/xdg/xdg-ubuntusway/waybar/config.jsonc"}" -s "${USER_STYLE:-"/etc/xdg/xdg-ubuntusway/waybar/style.css"}" &
