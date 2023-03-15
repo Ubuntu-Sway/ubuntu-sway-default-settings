b35a141db8e87e36986fb027e72376374757c1c1
Sunderland93 <samoilov.lex@gmail.com>
2023-03-16 01:08:24 +0400

    Add default swaylock config

--- common/etc/skel/.config/swaylock/lock.sh
+++ common/etc/skel/.config/swaylock/lock.sh
@@ -14,14 +14,4 @@ ffmpeg -i screen.png -vf "gblur=sigma=10, vignette=PI/5" -c:a copy screen-out.pn
 #Uses output image with Swaylock
 swaylock \
 --image screen-out.png \
---indicator-caps-lock \
---ignore-empty-password \
---show-failed-attempts \
---show-keyboard-layout \
---font=Ubuntu \
---inside-color 1b1b1b \
---inside-clear-color eeeeee \
---ring-color 3B758C \
---ring-clear-color 9fca56 \
---ring-ver-color 2980b9 \
 --daemonize
