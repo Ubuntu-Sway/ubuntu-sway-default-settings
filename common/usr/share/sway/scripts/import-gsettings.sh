963b907b4370e712423b861fb043e914f0fe6e10
Sunderland93 <samoilov.lex@gmail.com>
2023-03-25 11:54:46 +0400

    Disable window buttons on CSD windows

--- common/usr/share/sway/scripts/import-gsettings.sh
+++ common/usr/share/sway/scripts/import-gsettings.sh
@@ -5,6 +5,7 @@ config="${XDG_CONFIG_HOME:-$HOME/.config}/gtk-3.0/settings.ini"
 if [ ! -f "$config" ]; then exit 1; fi
 
 gnome_schema="org.gnome.desktop.interface"
+gnome_wm_schema="org.gnome.desktop.wm.preferences"
 gtk_theme="$(grep 'gtk-theme-name' "$config" | sed 's/.*\s*=\s*//')"
 icon_theme="$(grep 'gtk-icon-theme-name' "$config" | sed 's/.*\s*=\s*//')"
 cursor_theme="$(grep 'gtk-cursor-theme-name' "$config" | sed 's/.*\s*=\s*//')"
@@ -14,3 +15,4 @@ gsettings set "$gnome_schema" icon-theme "$icon_theme"
 gsettings set "$gnome_schema" cursor-theme "$cursor_theme"
 gsettings set "$gnome_schema" font-name "$font_name"
 gsettings set "$gnome_schema" color-scheme 'prefer_dark'
+gsettings set "$gnome_wm_schema" button-layout ""
