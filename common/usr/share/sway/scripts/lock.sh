f1d19069bea350242a0e8e5cc7582a537bba83ce
Sunderland93 <samoilov.lex@gmail.com>
2025-04-01 11:50:00 +0400

    Switch to gtklock, swaylock as a fallback

--- common/usr/share/sway/scripts/lock.sh
+++ common/usr/share/sway/scripts/lock.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
-USER_SCRIPT="$HOME/.config/swaylock/lock.sh"
-GLOB_SCRIPT="/usr/share/sway/scripts/swaylock.sh"
+# Swaylock run's with additional features, e.g. adding screenshot to background
+SWAYLOCK_USER_SCRIPT="$HOME/.config/swaylock/lock.sh"
+SWAYLOCK_GLOB_SCRIPT="/usr/share/sway/scripts/swaylock.sh"
 
-if [[ -x "$USER_SCRIPT" ]]; then
-    "$USER_SCRIPT"
-else
-    "$GLOB_SCRIPT"
+if [ -x "$(command -v gtklock)" ]; then
+    gtklock --daemonize --follow-focus --idle-hide --start-hidden
+elif [ -x "$(command -v swaylock)" ]; then
+    if [[ -x "$SWAYLOCK_USER_SCRIPT" ]]; then
+        "$SWAYLOCK_USER_SCRIPT"
+    else
+        "$SWAYLOCK_GLOB_SCRIPT"
+    fi
 fi
