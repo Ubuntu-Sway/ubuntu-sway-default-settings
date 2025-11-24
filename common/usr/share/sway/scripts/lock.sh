a8108845ed3d2df20d063febf457e2c06e8cdcf9
Sunderland93 <samoilov.lex@gmail.com>
2025-11-24 13:51:44 +0400

    Fix nwg-bar lock command

---
+++ common/usr/share/sway/scripts/lock.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Swaylock run's with additional features, e.g. adding screenshot to background
+SWAYLOCK_USER_SCRIPT="$HOME/.config/swaylock/lock.sh"
+SWAYLOCK_GLOB_SCRIPT="/usr/share/sway/scripts/swaylock.sh"
+
+if [ -x "$(command -v swaylock)" ]; then
+    if [[ -x "$SWAYLOCK_USER_SCRIPT" ]]; then
+        "$SWAYLOCK_USER_SCRIPT"
+    else
+        "$SWAYLOCK_GLOB_SCRIPT"
+    fi
+fi
